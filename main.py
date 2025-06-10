c966e6104f3aba0f2371dbc09e2ee9b83d26b76c
GH-57 <plue05007@gmail.com>
2025-06-10 18:58:56 +0900

    프로젝트 1일차

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from .routers import chat
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -6,12 +7,16 @@ app = FastAPI()
 # CORS 설정
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500"],  # PE가 실행되는 origin
+    allow_origins=["http://127.0.0.1:8000"],  # PE가 실행되는 origin
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
+app.include_router(chat.router, prefix="/api")
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello from FastAPI"}
+    return {"message": "오늘의 기분에 따른 잠언 추천 챗봇 API"}
+
+
